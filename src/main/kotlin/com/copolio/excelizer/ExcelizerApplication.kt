d9f852f22a3f2dc2f9ce1ff7fca711302a0bc58e
Luke <rlwjd1504@gmail.com>
2023-01-30 22:32:14 +0900

    Init project

---
+++ src/main/kotlin/com/copolio/excelizer/ExcelizerApplication.kt
@@ -0,0 +1,11 @@
+package com.copolio.excelizer
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ExcelizerApplication
+
+fun main(args: Array<String>) {
+    runApplication<ExcelizerApplication>(*args)
+}
